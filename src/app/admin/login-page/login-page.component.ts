f32a9b1620b391d9ea632fed66da708211cde934
Dmytro Myronenko <dim@uain.su>
2020-08-21 18:15:39 +0300

    small fix

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -28,8 +28,8 @@ export class LoginPageComponent implements OnInit {
     });
 
     this.form = new FormGroup({
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+      email: new FormControl('admin@email.com', [Validators.required, Validators.email]),
+      password: new FormControl('123456', [Validators.required, Validators.minLength(6)])
     });
   }
 
